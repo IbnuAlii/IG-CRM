f67fe445b8c7904acfe8de9d82800fd45ba56ac1
IbnuAlii <ibnuali600@gmail.com>
2026-06-17 12:50:59 +0300

    add: components/admin/quotes/quote-formatters.ts

---
+++ components/admin/quotes/quote-formatters.ts
@@ -0,0 +1,14 @@
+export function formatQuoteCurrency(value: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+export function formatQuoteDate(date: Date) {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }).format(date);
+}
